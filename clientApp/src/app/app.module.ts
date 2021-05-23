0f66133fdf8d5b86595dfb52c3fa6c548eb9a28d
Pawan <pawansharma261986@gmail.com>
2021-05-23 08:30:15 +0530

    Section 9 104 creating member cards

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +30,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
