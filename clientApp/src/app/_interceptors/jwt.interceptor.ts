c74456607839829533f86b26d26448507c080a3c
Pawan <pawansharma261986@gmail.com>
2021-05-23 09:34:11 +0530

    Section 9 107 using interceptor to send the token

---
+++ clientApp/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { User } from '../_models/user';
+import { take } from 'rxjs/operators';
+import { AccountsService } from '../_services/accounts.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountsService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let currentUser: User;
+
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      })
+    }
+
+    return next.handle(request);
+  }
+}
--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,9 +42,10 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     FormsModule,
     SharedModule
   ],
-  providers: [{
-    provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true
-  }],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:JwtInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
