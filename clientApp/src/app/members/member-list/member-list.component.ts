e10024f95f3894525a795591add27c5687674fb7
Pawan <pawansharma261986@gmail.com>
2021-06-07 18:29:03 +0530

    Section 13 160 Adding filter buttons to client

--- clientApp/src/app/members/member-list/member-list.component.ts
+++ clientApp/src/app/members/member-list/member-list.component.ts
@@ -18,6 +18,7 @@ members:Member[];
 pagination:Pagination;
 userParams:UserParams;
 user:User;
+genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
   constructor(private memberService: MembersService,private accountService:AccountsService) { 
     this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
       this.user=user
@@ -40,7 +41,10 @@ user:User;
       }
     )
   }
-
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
+    this.loadMembers();
+  }
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
     this.loadMembers();
