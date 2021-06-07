a243c566ac0c43a9f791f67398666046f991930a
Pawan <pawansharma261986@gmail.com>
2021-06-07 23:45:52 +0530

    Section 13 163 making last active filter more optimal

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -25,8 +25,8 @@ namespace API.Services
                   // Adding Claims
             var claims=new List<Claim>
           {
-              new Claim(
-                  JwtRegisteredClaimNames.NameId, user.UserName)
+              new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+              new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
           };
           //Creating Credentials
           var creds=new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
