79fc94c57363119f1eb91d4ca87667ceb94a9b09
Pawan <pawansharma261986@gmail.com>
2021-06-11 18:00:16 +0530

    Section 15 185 Getting the message from the repo

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,11 @@ namespace API.Helpers
                CreateMap<RegisterDto, AppUser>();
             //     .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             // CreateMap<Photo, PhotoDto>();
+                   CreateMap<Message, MessageDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
