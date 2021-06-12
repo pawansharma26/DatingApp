75e3f3f7bdd24c13e5c1cbe3f10ea3f9dbfa0f58
Pawan <pawansharma261986@gmail.com>
2021-06-12 15:20:00 +0530

    Section 15 191-1 Activating message tab

--- clientApp/src/app/members/member-detail/member-detail.component.ts
+++ clientApp/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
+  @ViewChild('memberTabs') memberTabs: TabsetComponent;
  member:Member;
  galleryOptions: NgxGalleryOptions[];
  galleryImages: NgxGalleryImage[];
