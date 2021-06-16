0071cd9700e4b4a8123af8bcd371bf0082cb2663
Pawan <pawansharma261986@gmail.com>
2021-06-16 20:50:47 +0530

    Section 16 212 Adding an admin guard

--- clientApp/src/app/app-routing.module.ts
+++ clientApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { hostViewClassName } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -11,6 +12,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { TdfvalidationComponent } from './tdfvalidation/tdfvalidation.component';
+import { AdminGuard } from './_guards/admin.guard';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
@@ -25,6 +27,7 @@ children:[
   {path:'member/edit',component:MemberEditComponent,canDeactivate:[PreventUnsavedChangesGuard]},
   {path:'lists',component:ListsComponent},
   {path:'messages',component:MessagesComponent},
+  {path:'admin',component:AdminPanelComponent,canActivate:[AdminGuard]},
 ]},
 {path: 'errors', component: TestErrorsComponent},
 {path: 'tdfvalidation', component: TdfvalidationComponent},
