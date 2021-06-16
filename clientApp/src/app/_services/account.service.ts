6e2979b46e388c59c3050c8510f74bdd99bf5f4a
Pawan <pawansharma261986@gmail.com>
2021-06-16 23:32:39 +0530

    Section 16 Complete

--- clientApp/src/app/_services/account.service.ts
+++ clientApp/src/app/_services/account.service.ts
@@ -1,39 +1,37 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-baseURL=environment.apiUrl;
-private currentUserSource=new ReplaySubject<User>(1);
-currentUser$=this.currentUserSource.asObservable();
-  constructor(private http:HttpClient) { }
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  login(model:any)
-  {
-    return this.http.post(this.baseURL + 'account/login', model).pipe(
-map((response:User)=>{
-const user=response;
-if(user)
-{
-  this.setCurrentUser(user);
-  
-}
-})
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
     )
   }
 
   register(model: any) {
-    return this.http.post(this.baseURL + 'account/register', model).pipe(
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          this.setCurrentUser(user);
+         this.setCurrentUser(user);
         }
-        return user;
       })
     )
   }
@@ -46,14 +44,12 @@ if(user)
     this.currentUserSource.next(user);
   }
 
-  logout()
-{
-localStorage.removeItem('user');
-this.currentUserSource.next(null);
-}
-getDecodedToken(token) {
-  return JSON.parse(atob(token.split('.')[1]));
-}
-}
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
 
-
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+}
